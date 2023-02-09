28b628f96084eb306d337ef5859ac908cfbb5ee1
ste@m1 <ste>
2023-02-09 13:30:54 +0100

    cql example

---
+++ db/cql/src/cql_exec.js
@@ -0,0 +1,26 @@
+// https://github.com/cqframework/cql-execution/tree/master/examples/node
+
+var cql = require("cql-execution");
+const measure = require('./age.json');
+
+const lib = new cql.Library(measure);
+const executor = new cql.Executor(lib);
+const psource = new cql.PatientSource([
+  {
+    id: '1',
+    recordType: 'Patient',
+    name: 'John Smith',
+    gender: 'M',
+    birthDate: '1980-02-17T06:15'
+  },
+  {
+    id: '2',
+    recordType: 'Patient',
+    name: 'Sally Smith',
+    gender: 'F',
+    birthDate: '2007-08-02T11:47'
+  }
+]);
+
+const result = executor.exec(psource);
+console.log(result.patientResults);
