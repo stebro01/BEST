8619dc1c15d604fbac853a6c6ddf76dd438a033c
ste@m1 <ste>
2023-02-09 15:11:37 +0100

    cql

--- db/cql/src/cql_exec.js
+++ db/cql/src/cql_exec.js
@@ -1,9 +1,9 @@
 // https://github.com/cqframework/cql-execution/tree/master/examples/node
 
 var cql = require("cql-execution");
-const measure = require('./age.json');
+const lib_age = require('./age.json');
 
-const lib = new cql.Library(measure);
+const lib = new cql.Library(lib_age);
 const executor = new cql.Executor(lib);
 const psource = new cql.PatientSource([
   {
@@ -11,16 +11,20 @@ const psource = new cql.PatientSource([
     recordType: 'Patient',
     name: 'John Smith',
     gender: 'M',
-    birthDate: '1980-02-17T06:15'
+    birthDate: '1980-02-17T06:15',
+    age: -10
+
   },
   {
     id: '2',
     recordType: 'Patient',
     name: 'Sally Smith',
     gender: 'F',
-    birthDate: '2007-08-02T11:47'
+    birthDate: '2007-08-02T11:47',
+    age: 20
   }
 ]);
 
 const result = executor.exec(psource);
-console.log(result.patientResults);
+console.log(result.patientResults)
+console.log(JSON.stringify(result, undefined, 2));
