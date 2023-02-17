6079f76376e37859de2a778e1ad663ab4da9df55
ste@m1 <ste>
2023-02-17 17:55:17 +0100

    minor change

--- db/app/src/components/cql/Card_CQL_concepts.vue
+++ db/app/src/components/cql/Card_CQL_concepts.vue
@@ -1,19 +1,33 @@
-<template>
- 
+<template><!-- CQL BUILD RULE -->
+  <div class="row justify-center" v-if="selected_data">
+    <q-card class="q-mb-xl my-item">
+      <q-card-section>Assoziierte Concepts <span>: CQL_ID = {{ selected_data.CQL_ID }}</span></q-card-section>
+      <q-card-section>
+        <q-table :rows="cql_list" :columns="columns" row-key="CONCEPT_CQL_ID" selection="single"
+          v-model:selected="selected_concept" :filter="filter">
 
-    <!-- CQL BUILD RULE -->
-    <div class="row justify-center" v-if="selected_data">
-      <q-card class="q-mb-xl my-item">
-        <q-card-section>Assoziierte Concepts <span >: CQL_ID = {{ selected_data.CQL_ID }}</span></q-card-section>
-        <q-card-section>
-          <q-table :rows="cql_list" :columns="columns" row-key="CONCEPT_CQL_ID" selection="single"
-            v-model:selected="selected_concept">
+          <template v-slot:top>
+            <q-btn-dropdown dense flat  icon="pending">
+              <q-list dense>
+                <q-item clickable v-close-popup @click="addRule()">
+                  <q-item-section avatar><q-icon name="add"/></q-item-section>
+                  <q-item-section>add</q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
 
-          </q-table>
-        </q-card-section>
-      </q-card>
-    </div>
+            <q-space />
+            <q-input borderless dense debounce="300" color="primary" v-model="filter">
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </template>
 
+        </q-table>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
@@ -23,11 +37,12 @@ export default {
 
   props: ['selected_data'],
 
-  components: {  },
+  components: {},
 
   data() {
     return {
       localData: undefined,
+      filter: null,
       cql_list: [],
       columns: [
         { name: 'CONCEPT_CQL_ID', align: 'center', label: 'ID', field: 'CONCEPT_CQL_ID', sortable: true },
@@ -57,14 +72,16 @@ export default {
   methods: {
     async loadData(data) {
       if (!data) return
-      this.cql_list = await this.$store.dispatch('searchDB', {table: 'CONCEPT_CQL_LOOKUP', query_string: {CQL_ID: data.CQL_ID, _view: true}})
-      
+      this.cql_list = await this.$store.dispatch('searchDB', { table: 'CONCEPT_CQL_LOOKUP', query_string: { CQL_ID: data.CQL_ID, _view: true } })
+    },
 
+    addRule(){
+      this.$q.notify('comming soon')
     }
-   
-    
-    
-   
+
+
+
+
 
     //ende methods
   }
