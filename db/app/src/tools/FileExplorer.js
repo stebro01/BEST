e0c8edb81a24689d0cd94f32db050614e37a370c
ste <ste@mbp.de>
2023-07-11 18:13:02 +0200

    minor changes

--- db/app/src/tools/FileExplorer.js
+++ db/app/src/tools/FileExplorer.js
@@ -1,93 +1,98 @@
 /**
  * FileExplorer class
  * @description This class is used to explore the file system
- * @example //Aufrufen des tests mit:
+ * @example // Run the tests with:
  * npm run test:unit test/jest/__tests__/FileExplorer.test.js
  * @param {*} pathModule
  * @param {*} fsModule
  * @returns {FileExplorer}
  */
 export class FileExplorer {
-  // constructor
-  // example: const explorer = new FileExplorer(path, fs);
-  constructor(pathModule, fsModule) {
-    this.path = pathModule;
-    this.fs = fsModule;
-    this.currentPath = "/";
-    this.availableDrives = this.detectDrives();
-  }
-
-  // methods
-
-  // setCurrentPath, example: explorer.setCurrentPath('/Users/ste/MyProjects/BEST/db/app')
-  setCurrentPath(folderPath) {
-    this.currentPath = folderPath;
-  }
-
-  // getCurrentPath, example: explorer.getCurrentPath()
-  getCurrentPath() {
-    return this.currentPath;
-  }
-
-  // getDirectoryContents, example: explorer.getDirectoryContents()
-  getDirectoryContents() {
-    const directoryPath = this.currentPath;
-
-    try {
-      const files = this.fs.readdirSync(directoryPath);
-      const contents = files.map((file) => {
-        const filePath = this.path.join(directoryPath, file);
-        const isDirectory = this.fs.lstatSync(filePath).isDirectory();
-        return {
-          name: file,
-          path: filePath,
-          isDirectory,
-        };
-      });
-      const RESULT = [];
-      contents.forEach((c) => {
-        if (c.isDirectory) RESULT.push(c.path);
-      });
-
-      return RESULT;
-    } catch (err) {
-      console.error("Error reading directory:", err);
-      return [];
-    }
-  }
-
-  // goUpOneLevel, example: explorer.goUpOneLevel()
-  goUpOneLevel() {
-    const currentPath = this.currentPath;
-    const parentDirectory = this.path.dirname(currentPath);
-    this.currentPath = parentDirectory;
-  }
-
-  // detectDrives, example: explorer.detectDrives()
-  detectDrives() {
-    const drives = [];
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    for (const letter of letters) {
-      const drivePath = `${letter}:\\`;
-      if (this.fs.existsSync(drivePath)) {
-        drives.push(drivePath);
+    constructor(pathModule, fsModule) {
+      this.path = pathModule;
+      this.fs = fsModule;
+      this.currentPath = "/";
+      this.availableDrives = this.detectDrives();
+  
+      if (process.platform === "win32" && this.availableDrives.length > 0) {
+        this.currentPath = this.availableDrives[0];
       }
     }
-
-    return drives;
+  
+    // Set the current path
+    // Example: explorer.setCurrentPath('/Users/ste/MyProjects/BEST/db/app')
+    setCurrentPath(folderPath) {
+      this.currentPath = folderPath;
+    }
+  
+    // Get the current path
+    // Example: explorer.getCurrentPath()
+    getCurrentPath() {
+      return this.currentPath;
+    }
+  
+    // Get the directory contents
+    // Example: explorer.getDirectoryContents()
+    getDirectoryContents() {
+      const directoryPath = this.currentPath;
+  
+      try {
+        const files = this.fs.readdirSync(directoryPath);
+        const directoryContents = files.map((file) => {
+          const filePath = this.path.join(directoryPath, file);
+          const isDirectory = this.fs.lstatSync(filePath).isDirectory();
+          return {
+            name: file,
+            path: filePath,
+            isDirectory,
+          };
+        });
+        const result = [];
+        directoryContents.forEach((content) => {
+          if (content.isDirectory) result.push(content.path);
+        });
+  
+        return result;
+      } catch (err) {
+        console.error("Error reading directory:", err);
+        return [];
+      }
+    }
+  
+    // Go up one level in the directory
+    // Example: explorer.goUpOneLevel()
+    goUpOneLevel() {
+      this.currentPath = this.path.dirname(this.currentPath);
+    }
+  
+    // Detect available drives
+    // Example: explorer.detectDrives()
+    detectDrives() {
+      const drives = [];
+      const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+      for (const letter of letters) {
+        const drivePath = `${letter}:\\`;
+        if (this.fs.existsSync(drivePath)) {
+          drives.push(drivePath);
+        }
+      }
+  
+      return drives;
+    }
+  
+    // Get info about the file explorer
+    // Example: explorer.info()
+    info() {
+      const currentPath = this.currentPath;
+      const availableDrives = this.availableDrives;
+      const directoryContents = this.getDirectoryContents();
+  
+      return {
+        currentPath,
+        availableDrives,
+        directoryContents,
+      };
+    }
   }
-
-  // info, example: explorer.info()
-  info() {
-    const currentPath = this.currentPath;
-    const availableDrives = this.availableDrives;
-    const directoryContents = this.getDirectoryContents();
-
-    return {
-      currentPath,
-      availableDrives,
-      directoryContents,
-    };
-  }
-}
+  
--- db/app/test/jest/__tests__/FileExplorer.test.js
+++ db/app/test/jest/__tests__/FileExplorer.test.js
@@ -36,52 +36,52 @@ describe('Teste RAW import', () => {
     console.log(explorer.info())
   })
 
-  // it (`Anzeige von Ordnern: getDirectoryContents`, async () => {
-  //   const res = explorer.getDirectoryContents()
-  //   console.log('getDirectoryContents:', res)
-  //   expect(res).not.toBeUndefined()
-  // })
+  it (`Anzeige von Ordnern: getDirectoryContents`, async () => {
+    const res = explorer.getDirectoryContents()
+    console.log('getDirectoryContents:', res)
+    expect(res).not.toBeUndefined()
+  })
 
-  // it (`can change the folder and list the contents`, async () => {  
-  //   explorer.setCurrentPath('/Users/ste/MyProjects/BEST/db/app')
-  //   const res = explorer.getDirectoryContents()
-  //   expect(res).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', res)
-  // })
+  it (`can change the folder and list the contents`, async () => {  
+    explorer.setCurrentPath('/Users/ste/MyProjects/BEST/db/app')
+    const res = explorer.getDirectoryContents()
+    expect(res).not.toBeUndefined()
+    console.log('getDirectoryContents:', res)
+  })
 
-  // it (`can go up one level`, async () => {  
-  //   explorer.setCurrentPath('/Users/ste/MyProjects/BEST/db/app')
-  //   const res = explorer.getDirectoryContents()
-  //   expect(res).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', res)
+  it (`can go up one level`, async () => {  
+    explorer.setCurrentPath('/Users/ste/MyProjects/BEST/db/app')
+    const res = explorer.getDirectoryContents()
+    expect(res).not.toBeUndefined()
+    console.log('getDirectoryContents:', res)
 
-  //   // go up one level
-  //   explorer.goUpOneLevel()
-  //   const res2 = explorer.getDirectoryContents()
-  //   expect(res2).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', res2)
+    // go up one level
+    explorer.goUpOneLevel()
+    const res2 = explorer.getDirectoryContents()
+    expect(res2).not.toBeUndefined()
+    console.log('getDirectoryContents:', res2)
 
-  //   // go up to root
-  //   explorer.setCurrentPath('/Users/ste/MyProjects/')
-  //   explorer.goUpOneLevel()
-  //   var resup = explorer.getDirectoryContents()
-  //   expect(resup).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', resup)
-  //   explorer.goUpOneLevel()
-  //   var resup = explorer.getDirectoryContents()
-  //   expect(resup).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', resup)
-  //   explorer.goUpOneLevel()
-  //   var resup = explorer.getDirectoryContents()
-  //   expect(resup).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', resup)
-  //   //should not work, should be root
-  //   explorer.goUpOneLevel()
-  //   var resup = explorer.getDirectoryContents()
-  //   expect(resup).not.toBeUndefined()
-  //   console.log('getDirectoryContents:', resup)
+    // go up to root
+    explorer.setCurrentPath('/Users/ste/MyProjects/')
+    explorer.goUpOneLevel()
+    var resup = explorer.getDirectoryContents()
+    expect(resup).not.toBeUndefined()
+    console.log('getDirectoryContents:', resup)
+    explorer.goUpOneLevel()
+    var resup = explorer.getDirectoryContents()
+    expect(resup).not.toBeUndefined()
+    console.log('getDirectoryContents:', resup)
+    explorer.goUpOneLevel()
+    var resup = explorer.getDirectoryContents()
+    expect(resup).not.toBeUndefined()
+    console.log('getDirectoryContents:', resup)
+    //should not work, should be root
+    explorer.goUpOneLevel()
+    var resup = explorer.getDirectoryContents()
+    expect(resup).not.toBeUndefined()
+    console.log('getDirectoryContents:', resup)
 
-  // })
+  })
 
   
 })
